f4b5949a43810b384a664a776f7e3f69499684bc
sagar.ranganath <sagar.ranganath@tu-ilmenau.de>
2017-06-06 19:45:02 +0200

    Intermediate

---
+++ Hello_World/Hello_World/MainActivity.cs
@@ -0,0 +1,19 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace Hello_World
+{
+    [Activity(Label = "Hello_World", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+             SetContentView (Resource.Layout.Main);
+        }
+    }
+}
+
