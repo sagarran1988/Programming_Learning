31c72259490d3a93fbbf7703069b36cc7d1d2a27
sagar.ranganath <sagar.ranganath@tu-ilmenau.de>
2017-06-06 16:14:50 +0200

    Final commit

---
+++ First_Space/First_Space/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace First_Space
+{
+    class namespace_cl
+    {
+        public void func()
+        {
+            Console.WriteLine("Inside first_space");
+        }
+    }
+}
+namespace Second_Space
+{
+    class namespace_cl
+    {
+        public void func()
+        {
+            Console.WriteLine("Inside second_space");
+        }
+    }
+}
+class TestClass
+{
+    static void Main(string[] args)
+    {
+        First_Space.namespace_cl fc = new First_Space.namespace_cl();
+        Second_Space.namespace_cl sc = new Second_Space.namespace_cl();
+        fc.func();
+        sc.func();
+        Console.ReadKey();
+    }
+}
